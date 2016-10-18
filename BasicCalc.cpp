22989f675a24af0191f7c0a804707fbea8fb7381
alpgc <alpgc@cin.ufpe.br>
2016-10-18 14:30:46 -0300

    init3

---
+++ BasicCalc.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <GL/gl.h>
+using namespace std;
+#define PX first
+#define PY second 
+typedef pair<double,double> pt;
+pt FindBezierPt(vector<pt> v,double t){
+	if(v.size()==1)return v[0];
+	vector<pt> aux;
+	for(int i=0;i<v.size()-1;i++){
+		aux.push_back(make_pair(v[i].PX*t+v[i+1].PX*(1-t),v[i].PY*t+v[i+1].PY*(1-t)));
+	}
+	return FindBezierPt(aux,t);
+}
--- project.cpp
+++ project.cpp
@@ -1,12 +1,2 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define PX first
-#define PY second 
-typedef pair<double,double> pt;
-pt FindBezierPt(vector<pt> v,double t){
-	if(v.size()==1)return v[0];
-	vector<pt> aux;
-	for(int i=0;i<v.size()-1;i++){
-		aux.push_back(make_pair(v[i].PX*t+v[i+1].PX*(1-t),v[i].PY*t+v[i+1].PY*(1-t)));
-	}
-}
