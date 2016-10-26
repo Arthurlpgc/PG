bf4aee1efb10aecf5f6a7ff2dc340b3ce232722d
alpgc <alpgc@cin.ufpe.br>
2016-10-26 16:38:01 -0300

    Panel and stuff

--- BasicCalc.cpp
+++ BasicCalc.cpp
@@ -25,41 +25,45 @@ vector<pt> Casteljau(vector<pt> v,int numPoints){
 }
 
 //Global variables
-vector<pt> cntline,BezierCurve;
-int pivotCp;
-bool delta;
+vector<pt> ControlPoints,BezierCurve;
+int SelectedPoint;
+double StartingX,StartingY;
 
 //events
 void mbpressed(GLFWwindow* window, int button, int action, int mods){
 	double xpos, ypos;
 	glfwGetCursorPos(window, &xpos, &ypos);
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS){
-    	//point drag
-        for(int i=0;i<cntline.size();i++){
-			if(pow(fabs(xpos-cntline[i].first),2)+pow(fabs(ypos-cntline[i].second),2)<3){
-				pivotCp=i;
-				delta=0;
-				return;
-			}        	
-        }
-        //point creation on line
-        //point creation at the end
-        cntline.push_back(make_pair(xpos,ypos));
-        BezierCurve=Casteljau(cntline,100);
-    }else {
-    	if(~pivotCp&&delta==0){
-    		cntline.erase(cntline.begin()+pivotCp);
-    		BezierCurve=Casteljau(cntline,100);
-    	}
-    	pivotCp=-1;
-    }
+	if(xpos<480){//if on screen area
+	    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS){
+	    	//point drag
+	        for(int i=0;i<ControlPoints.size();i++){
+				if(pow(fabs(xpos-ControlPoints[i].first),2)+pow(fabs(ypos-ControlPoints[i].second),2)<3){
+					SelectedPoint=i;
+					StartingX=xpos;
+					StartingY=ypos;
+					return;
+				}        	
+	        }
+	        //point creation on line
+	        //point creation at the end
+	        ControlPoints.push_back(make_pair(xpos,ypos));
+	        BezierCurve=Casteljau(ControlPoints,100);
+	    }else {
+	    	if(~SelectedPoint&&fabs(xpos-StartingX)+fabs(ypos-StartingY)<8){
+	    		ControlPoints.erase(ControlPoints.begin()+SelectedPoint);
+	    		BezierCurve=Casteljau(ControlPoints,100);
+	    	}
+	    	SelectedPoint=-1;
+	    }
+	}else{//if on panel area
+
+	}
 }
 void mmove(GLFWwindow* window, double xpos, double ypos){
-	if(~pivotCp){
-		cntline[pivotCp].first=xpos;
-		cntline[pivotCp].second=ypos;
-		delta=1;
-		BezierCurve=Casteljau(cntline,100);
+	if(~SelectedPoint){
+		ControlPoints[SelectedPoint].first=xpos;
+		ControlPoints[SelectedPoint].second=ypos;
+		BezierCurve=Casteljau(ControlPoints,100);
 	}
 }
 
@@ -67,18 +71,18 @@ void mmove(GLFWwindow* window, double xpos, double ypos){
 void makeLines(vector<pt> v){
 	for(int i=1;i<v.size();i++){
     	glBegin(GL_LINES);
-			glVertex2f(float(v[i].first/320.0-1.0),float(-v[i].second/240.0+1.0));
-			glVertex2f(float(v[i-1].first/320.0-1.0), float(-v[i-1].second/240.0+1.0));
+			glVertex2f(float(v[i].first/360.0-1.0),float(-v[i].second/240.0+1.0));
+			glVertex2f(float(v[i-1].first/360.0-1.0), float(-v[i-1].second/240.0+1.0));
 		glEnd();
     }
 }
 
 //main
 int main(void){	
-	pivotCp=-1;
+	SelectedPoint=-1;
     GLFWwindow* window;
     if (!glfwInit())return -1;//glfw starting
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);//window startup
+    window = glfwCreateWindow(720, 480, "P3.1", NULL, NULL);//window startup
     if (!window){
         glfwTerminate();
         return -1;
@@ -87,14 +91,16 @@ int main(void){
     glfwSetMouseButtonCallback(window, mbpressed);//setting callback for mouse button
     glfwSetCursorPosCallback(window, mmove);//setting callback for mouse move
     while (!glfwWindowShouldClose(window)){//render loop
-        glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
+        glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glColor3f(0, 0, 0.10);
+        glRectf(0.3333,-1.0,1.0,1.0);
         glLineWidth(0.5);
 		glColor3f(1.0, 1.0, 1.0);
         makeLines(BezierCurve);
         glLineWidth(1.5);
         glColor3f(0.0, 1.0, 1.0);
-        makeLines(cntline);
+        makeLines(ControlPoints);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
