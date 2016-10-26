0014cc5fcf29830b31f599fe0ec019e7ef7291c5
alpgc <alpgc@cin.ufpe.br>
2016-10-26 14:35:57 -0300

    glfw funct

--- BasicCalc.cpp
+++ BasicCalc.cpp
@@ -1,22 +1,37 @@
+#include <GLFW/glfw3.h>
 #include <bits/stdc++.h>
-using namespace std;
-#define PX first
-#define PY second 
-typedef pair<double,double> pt;
-pt FindBezierPt(vector<pt> v,double t){
-	while(v.size()>1){
-		for(int i=0;i<v.size()-1;i++){
-			v[i]=(make_pair(v[i].PX*t+v[i+1].PX*(1-t),v[i].PY*t+v[i+1].PY*(1-t)));
-		}
-		v.pop_back();
-	}
-	return v[0];
-}
-vector<pt> Casteljau(vector<pt> v,int numPoints){
-	assert(numPoints>1);
-	vector<pt> ret;
-	for(int i=0;i<numPoints;i++){
-		ret.push_back(FindBezierPt(v,double(i)/double(numPoints-1)));
-	}
-	return ret;
+int main(void)
+{	std::cout<<"oi";
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
